b077f45435cb77cbb41eec0f5e069be357b979c6
Waqar Ahmad Khan <waqarbinmuhammad@gmail.com>
2022-07-03 00:01:51 +0500

    Created Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Employee</h1>
-    </div>
+    <>
+      <Navbar/>
+    </>
+    
   );
 }
 
